bb91ed8f76415e3e2f96d4ecba2a7ba039bb234e
ranaakash0003 <rana.akash0003@gmail.com>
2020-03-21 01:30:20 +0600

    media query added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
  const Footer = () => {
     return (
         <div>
-            <p style={{marginTop: "50px",textAlign:"center"}}>Copyright © Rana Akash</p>
+            <p style={{marginTop: "50px",textAlign:"center"}}>developed by <a href="https://www.facebook.com/raaz.akash">rana akash</a></p>
         </div>
     )
 }

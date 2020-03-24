a505417e6735950fee7f8123e223e78c4eac33cb
ranaakash0003 <rana.akash0003@gmail.com>
2020-03-24 22:10:52 +0600

    mets tsg sdded

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Header from "./components/header/header";
 import Home from "./pages/Home";
 import Datatable from "./components/datatable/Datatable";
 import Footer from "./components/footer/Footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="app-container">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <link rel="canonical" href="https://recovid.netlify.com/" />
+      </Helmet>
       <Header />
       <Home />
       <Datatable />
